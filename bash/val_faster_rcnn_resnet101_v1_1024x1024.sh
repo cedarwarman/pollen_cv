597ab53dbee128a50b8f71983d13ad0700dce0b3
cedarwarman <cedardalewarman@gmail.com>
2022-10-24 21:13:35 +0000

    Fine tune config

--- bash/val_faster_rcnn_resnet101_v1_1024x1024.sh
+++ bash/val_faster_rcnn_resnet101_v1_1024x1024.sh
@@ -5,8 +5,10 @@ cd /home/git/models/research/
 # From the tensorflow/models/research/ directory
 PIPELINE_CONFIG_PATH="/home/git/pollen_cv/config/faster_rcnn_resnet101_v1_1024x1024.config"
 MODEL_DIR="/media/volume/sdb/models/2022-10-24_resnet_augmentations"
+CHECKPOINT_DIR="/media/volume/sdb/models/2022-10-24_resnet_augmentations"
 python object_detection/model_main_tf2.py \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
     --model_dir=${MODEL_DIR} \
+    --checkpoint_dir=${CHECKPOINT_DIR} \
     --alsologtostderr
 #   --checkpoint_every_n=<int>
