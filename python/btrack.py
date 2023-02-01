261794b53d1d855ab5d639ab89adf5346af1d948
cedarwarman <cedardalewarman@gmail.com>
2023-01-31 17:09:35 -0700

    Start working on btrack object dictionary.

--- python/btrack.py
+++ python/btrack.py
@@ -5,8 +5,10 @@
 
 import pandas as pd
 from pathlib import Path
+import btrack
 
-def load_tsv(file_name:str)->pd.DataFrame:
+
+def load_tsv(file_name: str) -> pd.DataFrame:
     """
     Load a tsv file into a pandas dataframe, based on the location of the 
     python script. The tsv is in a directory called data that is in the same 
@@ -39,7 +41,8 @@ def load_tsv(file_name:str)->pd.DataFrame:
 
     return df
 
-def subset_by_confidence_score(df:pd.DataFrame, confidence_score:float)->pd.DataFrame:
+
+def subset_by_confidence_score(df: pd.DataFrame, confidence_score: float) -> pd.DataFrame:
     """
     Subset a dataframe based on a confidence score.
 
@@ -58,10 +61,11 @@ def subset_by_confidence_score(df:pd.DataFrame, confidence_score:float)->pd.Data
     subset_df = df[df["score"] >= confidence_score]
     return subset_df
 
-def calculate_centroid(df:pd.DataFrame)->pd.DataFrame:
+
+def calculate_centroid(df: pd.DataFrame) -> pd.DataFrame:
     """
     Calculate the centroid value for each row of a dataframe with columns 
-    titled "ymin", "xmin", "ymax", "xmax"
+    titled "ymin", "xmin", "ymax", "xmax".
 
     Parameters:
     ----------
@@ -77,6 +81,42 @@ def calculate_centroid(df:pd.DataFrame)->pd.DataFrame:
     df["centroid_y"] = df.apply(lambda row: (row["ymin"] + row["ymax"]) / 2, axis=1)
     return df
 
+#def add_rows_to_btrack(df: pd.DataFrame) -> list[PyTrackObject]:
+def add_rows_to_btrack(df: pd.DataFrame) -> list:
+
+    """
+    Create btrack PyTrackObjects from a Pandas DataFrame.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Input dataframe with calculated centroids and confidence score cutoffs
+
+    Returns
+    -------
+    list
+        list of PyTrackObjects, one for each row of the input DataFrame
+    """
+
+    btrack_objects = []
+    id_counter = 0
+
+    for row in df.itertuples(index=False):
+        row_dict = {}
+        row_dict['ID'] = id_counter
+        row_dict['t'] = getattr(row, "timepoint")
+        row_dict['x'] = getattr(row, "centroid_x")
+        row_dict['y'] = getattr(row, "centroid_y")
+        row_dict['z'] = 0.
+        #row_dict['rpn_score'] =
+        #row_dict['rpn_obj_type'] =
+        #row_dict['rpn_box'] =
+
+        btrack_objects.append(row_dict)
+        id_counter += 1
+
+    return btrack_objects
+
 
 def main():
     df = load_tsv("2022-01-05_run1_26C_D2_t082_stab_predictions.tsv")
@@ -85,6 +125,9 @@ def main():
     print(df)
     df = calculate_centroid(df)
     print(df)
+    btrack_objects = add_rows_to_btrack(df)
+    print(btrack_objects)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
