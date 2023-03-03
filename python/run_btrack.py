e7ed94f18637848d8987ffcc443d957f0a093c40
cedarwarman <cedardalewarman@gmail.com>
2023-03-03 15:46:50 -0700

    Clean up file loading and imports.

--- python/run_btrack.py
+++ python/run_btrack.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-import btrack.btypes
 
-# Trying out btrack for tracking pollen tubes.
-# https://github.com/quantumjot/BayesianTracker
+"""
+Trying out btrack for tracking pollen tubes.
 
-from typing import List, Tuple, Union
-import pandas as pd
-import numpy as np
-from skimage.io import imread
+Original btrack repository is located here:
+https://github.com/quantumjot/BayesianTracker
+"""
+
 from pathlib import Path
-import glob
+from typing import List, Tuple, Union
+
 import btrack
-from btrack.btypes import PyTrackObject
+import btrack.btypes
 import napari
+import numpy as np
+import pandas as pd
+from skimage.io import imread
 
 
 def load_tsv(
@@ -34,6 +37,10 @@ def load_tsv(
         The tsv file loaded into a pandas dataframe.
 
     """
+
+    # Add the rest of the file name based on the inference output convention
+    file_name = file_name + "_t082_stab_predictions.tsv"
+
     # Get the path of the current script
     current_script_path = Path(__file__).resolve()
 
@@ -106,7 +113,7 @@ def calculate_centroid(
 
 def add_rows_to_btrack(
     df: pd.DataFrame
-) -> List[PyTrackObject]:
+) -> List[btrack.btypes.PyTrackObject]:
     """Create btrack PyTrackObjects from a Pandas DataFrame.
 
     Parameters
@@ -116,7 +123,7 @@ def add_rows_to_btrack(
 
     Returns
     -------
-    btrack_objects: List[PyTrackObject]
+    btrack_objects: List[btrack.btypes.PyTrackObject]
         List of PyTrackObjects, one for each row of the input DataFrame.
 
     """
@@ -135,7 +142,7 @@ def add_rows_to_btrack(
         # row_dict['rpn_obj_type'] =
         # row_dict['rpn_box'] =
 
-        obj = PyTrackObject.from_dict(row_dict)
+        obj = btrack.btypes.PyTrackObject.from_dict(row_dict)
         btrack_objects.append(obj)
         id_counter += 1
 
@@ -143,13 +150,13 @@ def add_rows_to_btrack(
 
 
 def run_tracking(
-    btrack_objects: Union[List[PyTrackObject], np.ndarray]
+    btrack_objects: Union[List[btrack.btypes.PyTrackObject], np.ndarray]
 ) -> Tuple[np.ndarray, dict, dict]:
     """Run the btrack tracking algorithm on a list of PyTrackObjects.
 
     Parameters
     ----------
-    btrack_objects : Union[List[PyTrackObject], np.ndarray]
+    btrack_objects : Union[List[btrack.btypes.PyTrackObject], np.ndarray]
         A list of PyTrackObjects created with the add_rows_to_btrack function.
 
     Returns
@@ -193,7 +200,8 @@ def visualize_tracks(
         track_data: np.ndarray,
         track_properties: dict,
         track_graph: dict,
-        background_images: Union[str, Path]
+        background_images: str,
+        show_bounding_boxes: bool
 ) -> None:
     """Visualize btrack output with source images as background.
 
@@ -205,8 +213,10 @@ def visualize_tracks(
         Output from the run_tracking function.
     track_graph : dict
         Output from the run_tracking function.
-    background_images : Union[str, Path]
-        Path to the images used for the background.
+    background_images : str
+        Name of the images used for the background.
+    show_bounding_boxes : bool
+        Whether or not to load the background images with bounding boxes.
 
     Returns
     -------
@@ -220,7 +230,22 @@ def visualize_tracks(
     print("Adding images")
     image_series = []
 
-    for image_path in sorted(glob.glob(background_images)):
+    # Formatting the image background image string
+    if show_bounding_boxes:
+        background_images = background_images + "_stab_inference"
+    else:
+        background_images = background_images + "_stab"
+
+    # Getting the path of the background images
+    current_script_path = Path(__file__).resolve()
+
+    # Navigate to the parent directory
+    parent_dir = current_script_path.parent.parent
+
+    # Navigate to the data directory
+    image_dir = parent_dir / "data" / "btrack_visualization_images" / background_images
+
+    for image_path in sorted(image_dir.glob("*.jpg")):
         image = imread(image_path)
         image_array = np.asarray(image)
         image_series.append(image_array)
@@ -248,32 +273,21 @@ def main():
     pd.set_option('display.max_columns', None)
 
     # Some example image sequence inference files
-    df = load_tsv("2022-03-03_run1_26C_C2_t082_stab_predictions.tsv")
-    # df = load_tsv("2022-03-07_run1_26C_B5_t082_stab_predictions.tsv")
-    # df = load_tsv("2022-03-07_run1_26C_C2_t082_stab_predictions.tsv")
+    image_seq_name = "2022-03-03_run1_26C_C2"
+    # image_seq_name = "2022-03-07_run1_26C_B5"
+    # image_seq_name = "2022-03-07_run1_26C_C2"
 
-    print("DF is: ")
-    print(df)
-
+    # Loading and processing the dataframe
+    df = load_tsv(image_seq_name)
     df = subset_df(df, 0.35)
     df = calculate_centroid(df)
-    print(df.head(n=5))
+
+    # Adding to btrack and calculating tracks
     btrack_objects = add_rows_to_btrack(df)
-    # print(btrack_objects[0])
     data, properties, graph = run_tracking(btrack_objects)
 
-    # Image sequences to go along with the inference, for visualization.
-    image_dir = "/Users/warman/git/pollen_cv/data/btrack_visualization_images/2022-03-03_run1_26C_C2_inference/*.jpg"
-    # image_dir = "/Users/warman/Desktop/Science/computer_vision/btrack/2022-03-03_run1_26C_C2_stab/*.jpg"
-
-    # image_dir = "/Users/warman/Desktop/Science/computer_vision/btrack/2022-03-07_run1_26C_B5_inference/*.jpg"
-    # image_dir = "/Users/warman/Desktop/Science/computer_vision/btrack/2022-03-07_run1_26C_B5_stab/*.jpg"
-
-    # image_dir = "/Users/warman/Desktop/Science/computer_vision/btrack/2022-03-07_run1_26C_C2_inference/*.jpg"
-    # image_dir = "/Users/warman/Desktop/Science/computer_vision/btrack/2022-03-07_run1_26C_C2_stab/*.jpg"
-
     # Viewing tracks and images with Napari
-    visualize_tracks(data, properties, graph, image_dir)
+    visualize_tracks(data, properties, graph, image_seq_name, show_bounding_boxes=True)
 
     print("All done")
 
