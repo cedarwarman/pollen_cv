95ac41c152150bfb721f4010fec4c04ad0fda4d0
cedarwarman <cedardalewarman@gmail.com>
2023-03-28 20:44:33 +0000

    Add some new configs and do some stuff with second camera.

--- bash/train_centernet_hourglass104_1024x1024.sh
+++ bash/train_centernet_hourglass104_1024x1024.sh
@@ -3,8 +3,8 @@
 cd /home/git/models/research/
 
 # From the tensorflow/models/research/ directory
-PIPELINE_CONFIG_PATH="/home/git/pollen_cv/config/centernet_hourglass104_pollen_classes_2023-03-23.config"
-MODEL_DIR="/media/volume/sdb/models/2023-03-23_centernet_pollen"
+PIPELINE_CONFIG_PATH="/home/git/pollen_cv/config/centernet_hourglass104_all_classes_2023-03-27.config"
+MODEL_DIR="/media/volume/sdb/models/2023-03-27_centernet_all"
 python object_detection/model_main_tf2.py \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
     --model_dir=${MODEL_DIR} \
