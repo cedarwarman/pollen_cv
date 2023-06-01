2856efe95781ba019fc6c0710f3dd639fdf4b70b
cedarwarman <cedardalewarman@gmail.com>
2023-06-01 22:09:03 +0000

    Config stuff

--- bash/val_centernet_hourglass104_1024x1024.sh
+++ bash/val_centernet_hourglass104_1024x1024.sh
@@ -3,9 +3,9 @@
 cd /home/git/models/research/
 
 # From the tensorflow/models/research/ directory
-PIPELINE_CONFIG_PATH="/home/git/pollen_cv/config/2023-04-18_centernet_two_tube_tip.config"
-MODEL_DIR="/media/volume/sdb/models/2023-04-18_pub_models/two_tube_tip"
-CHECKPOINT_DIR="/media/volume/sdb/models/2023-04-18_pub_models/two_tube_tip"
+PIPELINE_CONFIG_PATH="/home/git/pollen_cv/config/2023-04-18_centernet_both_tube_tip.config"
+MODEL_DIR="/media/volume/sdb/models/2023-04-18_pub_models/combined_tube_tip"
+CHECKPOINT_DIR="/media/volume/sdb/models/2023-04-18_pub_models/combined_tube_tip"
 python object_detection/model_main_tf2.py \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
     --model_dir=${MODEL_DIR} \
